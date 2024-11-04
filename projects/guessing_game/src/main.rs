4d763f1d526e969b0646d8824bcd8d52a17094db
pepukrit <pepukrit@hotmail.com>
2024-11-04 09:09:02 +0700

    Handled an error and hide logging secret_number on the screen

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number: {}", secret_number);
+    //println!("Secret number: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -19,7 +19,10 @@ fn main() {
 
         println!("You guessed: {}", guess);
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
