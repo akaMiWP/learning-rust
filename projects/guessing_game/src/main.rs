f832681afd7b4d3062d75801c51fdc7d76ec1ca5
pepukrit <pepukrit@hotmail.com>
2024-11-04 09:14:45 +0700

    Removed the line

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    //println!("Secret number: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
