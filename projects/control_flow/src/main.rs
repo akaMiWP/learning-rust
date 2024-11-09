1c52246724366b2780ed5393669e7b6fa6bf4b3b
akaMiWP <bangkokappdevelopment@gmail.com>
2024-11-09 17:27:18 +0700

    Finished the chapter

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -6,4 +6,13 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    loop {
+        println!("again!");
+    }
 }
